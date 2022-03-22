a4682252982658ca90ef2d818bf8bc495baaa88f
ChaseDurand <chase.durand@gmail.com>
2022-03-22 12:32:41 -0500

    Added header.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 const int sampleRate = 44100;
 const int bitDepth = 16;
+const int channels = 1;
 
 class SineOscillator {
 
@@ -21,21 +22,53 @@ class SineOscillator {
 
 };
 
+void writeToFile(std::ofstream &file, int value, int size) {
+    file.write(reinterpret_cast<const char*>(&value), size);
+}
+
 int main() {
     SineOscillator sineOscillator(440, 0.5);
     int duration = 2; // Duration in seconds
 
     std::ofstream audioFile;
-    audioFile.open("waveform", std::ios::binary);
+    audioFile.open("waveform.wav", std::ios::binary);
+
+    // Header chunk
+    audioFile << "RIFF"; // Chunk ID
+    audioFile << "----"; // Placeholder for file size-8
+    audioFile << "WAVE";
+
+    // Format chunk
+    audioFile << "fmt "; // Chunk ID
+    writeToFile(audioFile, 16, 4); // Chunk data size
+    writeToFile(audioFile, 1, 2); // Compression Code
+    writeToFile(audioFile, channels, 2); // Number of channels
+    writeToFile(audioFile, sampleRate, 4); // Sample rate
+    writeToFile(audioFile, sampleRate * bitDepth * channels / 8, 4); // Average bytes per second
+    writeToFile(audioFile, bitDepth / 8, 2); // Block align
+    writeToFile(audioFile, bitDepth, 2); // Significant bits per sample
+
+    // Data chunk
+    audioFile << "data"; // Chunk ID
+    audioFile << "----"; // Size
+
+    int preAudioPosition = audioFile.tellp(); // Get current pointer position in stream
 
     auto maxAmp = pow(2, bitDepth - 1) - 1;
     for (int i = 0; i < sampleRate * duration; ++i) {
         auto sample = sineOscillator.process();
         int intSample = static_cast<int>(sample * maxAmp);
-        audioFile.write(reinterpret_cast<char*>(&intSample), 2);
+        writeToFile(audioFile, intSample, 2);
     }
 
-    audioFile.close();
+    int postAudioPosition = audioFile.tellp();
 
+    audioFile.seekp(preAudioPosition - 4);
+    writeToFile(audioFile, postAudioPosition - preAudioPosition, 4); // Data size (audio size)
+
+    audioFile.seekp(4, std::ios::beg);
+    writeToFile(audioFile, postAudioPosition - 8, 4); // Size in header (everything minus header)
+
+    audioFile.close();
     return 0;
 }
