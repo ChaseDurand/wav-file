7e0c487a06996d511381974c04ace65b04e27278
ChaseDurand <chase.durand@gmail.com>
2022-03-23 14:26:30 -0500

    Cleaned up buffer.

--- write.cpp
+++ write.cpp
@@ -5,10 +5,12 @@
 #include <ctime>
 #include <vector>
 #include <cstddef>
+#include <cstring>
 
 const int sampleRate = 44100;
 const int bitDepth = 16;
 const int channels = 2;
+const int duration = 2; // Duration in seconds
 
 class SineOscillator {
 public:
@@ -24,140 +26,77 @@ private:
     float frequency, amplitude, angle = 0.0f, offset = 0.0;
 };
 
-void addNumber(std::vector<char>& buf, int pos, int val) {
-    char a = (val >> 24) & 0xff;  // high-order (leftmost) byte: bits 24-31
-    char b = (val >> 16) & 0xff;  // next byte, counting from left: bits 16-23
-    char c = (val >>  8) & 0xff;  // next byte, bits 8-15
-    char d = val         & 0xff;  // low-order byte: bits 0-7
+class Buffer {
+public:
+    Buffer() {}
 
-    buf[pos] = d;
-    buf[pos+1] = c;
-    buf[pos+2] = b;
-    buf[pos+3] = a;
+    int size(){
+        return buf.size();
+    }
 
-    return;
-}
+    void add(const char* val) {
+        int length = strlen(val);
+        for(int i = 0; i < length; ++i) {
+            buf.push_back(val[i]);
+        }
+        return;
+    }
 
+    void add(int val, int size, int position = -1) {
+        int initialSize = size;
+        while(size > 0) {
+            char charVal = (val >> (initialSize - (size))*8) & 0xff;
+            if(position == -1) {
+                buf.push_back(charVal);
+            }
+            else {
+                buf[position+(initialSize-size)] = charVal;
+            }
+            size--;
+        }
+        return;
+    }
 
-// void writeToFile(std::ofstream &file, int value, int size) {
-//     file.write(reinterpret_cast<const char*>(&value), size);
-// }
+    auto begin() {
+        return buf.begin();
+    }
 
-// void appendToBuffer(std::vector<char>& buffer, const char* value) {
-//     for(auto iter = value.begin(); iter != value.end(); ++iter) {
-//         buffer.push_back(reinterpret_cast<const char*>(&value));
-//     }
-// }
-
-// std::vector<char> intToByteArray(int value, int size) {
-//     std::vector<char> result[size];
-//     for(int i = 0; i < size; ++i) {
-//         result[(size-1)-i] = static_cast<char> (value & 0x000000ff);
-//         value = value >> 8;
-//     }
-//     return result;
-// }
+    auto end() {
+        return buf.end();
+    }
+private:
+    std::vector<char> buf;
+};
 
 int main() {
     srand (static_cast <unsigned> (time(0)));
     SineOscillator sineOscillator(440, 0.5);
-    int duration = 2; // Duration in seconds
-
-    std::vector<char> buffer;
-
+    Buffer buffer;
 
     // Header chunk
-    // appendToBuffer(buffer, 'RIFF'); // Chunk ID
-    buffer.push_back('R');
-    buffer.push_back('I');
-    buffer.push_back('F');
-    buffer.push_back('F');
-
-    // appendToBuffer(buffer, '----'); // Placeholder for file size-8
-    buffer.push_back('-');
-    buffer.push_back('-');
-    buffer.push_back('-');
-    buffer.push_back('-');
-    // appendToBuffer(buffer, 'WAVE');
-    buffer.push_back('W');
-    buffer.push_back('A');
-    buffer.push_back('V');
-    buffer.push_back('E');
+    buffer.add("RIFF");
+    int totalSizePos = buffer.size(); // Note current position to later fill in size
+    buffer.add("----"); // Placeholder for file size (minus 8 byte header)
+    buffer.add("WAVE"); // Format
 
     // Format chunk
-    // appendToBuffer(buffer, 'fmt '); // Chunk ID
-    buffer.push_back('f');
-    buffer.push_back('m');
-    buffer.push_back('t');
-    buffer.push_back(' ');
-
-    // appendToBuffer(buffer, 16); // Chunk data size (16)
-    buffer.push_back(16);
-    buffer.push_back(0);
-    buffer.push_back(0);
-    buffer.push_back(0);
-    
-
-    // appendToBuffer(buffer, 1); // Compression Code (1 == PCM/uncompressed)
-    buffer.push_back(1);
-    buffer.push_back(0);
-    
-    
-    // appendToBuffer(buffer, channels); // Number of channels
-    buffer.push_back(channels);
-    buffer.push_back(0);
-    
-
-    // appendToBuffer(buffer, sampleRate, 4); // Sample rate
-    char sRa = (sampleRate) & 0xff;
-    char sRb = (sampleRate >>  8) & 0xff;
-    buffer.push_back(sRa);
-    buffer.push_back(sRb);
-    buffer.push_back(0);
-    buffer.push_back(0);
-    
-
-    // appendToBuffer(buffer, ); // Average bytes per second
-
-    char aBpSa = (sampleRate * bitDepth * channels / 8) & 0xff;
-    buffer.push_back(aBpSa);
-    aBpSa = ((sampleRate * bitDepth * channels / 8) >> 8) & 0xff;
-    buffer.push_back(aBpSa);
-    aBpSa = ((sampleRate * bitDepth * channels / 8) >> 16) & 0xff;
-    buffer.push_back(aBpSa);
-    aBpSa = ((sampleRate * bitDepth * channels / 8) >>24) & 0xff;
-    buffer.push_back(aBpSa);
-    
-
-    // appendToBuffer(buffer, channels * bitDepth / 8); // Block align
-    buffer.push_back(channels * bitDepth / 8);
-    buffer.push_back(0);
-    
-
-
-    // appendToBuffer(buffer, bitDepth); // Significant bits per sample
-    buffer.push_back(bitDepth);
-    buffer.push_back(0);
+    buffer.add("fmt "); // Chunk ID
+    buffer.add(16, 4); // Chunk data size (16)
+    buffer.add(1, 2); // Compression Code (1 == PCM/uncompressed)
+    buffer.add(channels, 2); // Number of channels
+    buffer.add(sampleRate, 4); // Sample rate
+    buffer.add(sampleRate * bitDepth * channels / 8, 4); // Average bytes per second
+    buffer.add(channels * bitDepth / 8, 2); // Block align
+    buffer.add(bitDepth, 2); // Significant bits per sample
     
     // Data chunk
-    // appendToBuffer(buffer, 'data'); // Chunk ID
-    buffer.push_back('d');
-    buffer.push_back('a');
-    buffer.push_back('t');
-    buffer.push_back('a');
+    buffer.add("data"); // Chunk ID
+    int dataChunkSizePos = buffer.size(); // Note current position to later fill in size
+    buffer.add("----"); // Placeholder for data size
 
+    int preDataSize = buffer.size(); // Note current size to later determine total data bytes
 
-    // appendToBuffer(buffer, '----'); // Size
-    buffer.push_back('-');
-    buffer.push_back('-');
-    buffer.push_back('-');
-    buffer.push_back('-');
-
-    // int preAudioPosition = audioFile.tellp(); // Get current pointer position in stream
-
-    int preAudioSize = buffer.size();
-
-    auto maxAmp = pow(2, bitDepth - 1) - 1;
+    auto maxAmp = pow(2, bitDepth - 1) - 1; // Scale sine wave from -1:+1 based on bit depth
     for (int i = 0; i < sampleRate * duration; ++i) {
         auto sample = sineOscillator.process();
         float noiseAmp = 0.00f; // Stereo noise level
@@ -166,19 +105,15 @@ int main() {
                 ( static_cast<float>( RAND_MAX / (2 * noiseAmp) ) );
             float sampleTmp = sample + noise;
             sampleTmp *= maxAmp;
-            int sampleInt = static_cast<int>(sampleTmp);
-            char c = (sampleInt >>  8) & 0xff;  // next byte, bits 8-15
-            char d = sampleInt         & 0xff;  // low-order byte: bits 0-7
-            buffer.push_back(d);
-            buffer.push_back(c);
+            buffer.add(static_cast<int>(sampleTmp), 2);
         }
     }
 
-    int postAudioSize = buffer.size();
-    int dataSize = postAudioSize - preAudioSize;
-    int chunkDataSize = postAudioSize - 8;
-    addNumber(buffer, 4, chunkDataSize);
-    addNumber(buffer, preAudioSize-4, dataSize);
+    // Add chunk data size (total size - 8 byte header)
+    buffer.add(buffer.size() - 8, 4, totalSizePos);
+
+    // Add format chunk data size
+    buffer.add(buffer.size() - preDataSize, 4, dataChunkSizePos);
 
     std::ofstream fout("output.wav");
     for(auto const& x : buffer) {
